2f962cdd72d01ae066756e7efe082e9cf0b29694
unknown <rreva.ptriii@gmail.com>
2025-09-29 15:50:31 +0800

    css path edit in app views

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,7 +10,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <!-- <link rel="stylesheet" href="{{ asset('public/css/style.css') }}"> -->
+     <link rel="stylesheet" href="/public/css/style.css">
+
 
     <!-- SweetAlert2 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css" rel="stylesheet">
